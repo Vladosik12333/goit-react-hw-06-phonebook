8a04c87db5e2b7d7c2b53de24ac0433f1bb0e340
Vlad <vvllaadd1233345@gmail.com>
2021-12-31 22:54:57 +0200

    fix(component contacts, contacts-reducer): transfer localeStor from component to reducer

--- src/redux/contacts/contacts-reducer.js
+++ src/redux/contacts/contacts-reducer.js
@@ -5,9 +5,16 @@ import { combineReducers } from '@reduxjs/toolkit';
 const dataUsers = JSON.parse(localStorage.getItem('contacts')) ?? [];
 
 const items = createReducer(dataUsers, {
-  [deleteContact]: (state, action) =>
-    state.filter(contact => contact.id !== action.payload),
-  [addContact]: (state, { payload }) => [...state, payload],
+  [deleteContact]: (state, action) => {
+    const newContacts = state.filter(contact => contact.id !== action.payload);
+    localStorage.setItem('contacts', JSON.stringify(newContacts));
+    return newContacts;
+  },
+  [addContact]: (state, { payload }) => {
+    const newContacts = [...state, payload];
+    localStorage.setItem('contacts', JSON.stringify(newContacts));
+    return newContacts;
+  },
 });
 
 const filter = createReducer('', {
